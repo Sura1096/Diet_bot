8e8f54a2fdc85db4c15bfd466fbc630fd7695150
Sura <suri.murodova@gmail.com>
2024-07-09 15:44:40 +0300

    Add a handler for the start command

---
+++ bot/handlers/commands_handlers.py
@@ -0,0 +1,13 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart, StateFilter
+from aiogram.fsm.state import default_state
+from ..lexicon.commands_lexicon import LEXICON
+
+
+router = Router()
+
+
+@router.message(CommandStart(), StateFilter(default_state))
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON['/start'])
